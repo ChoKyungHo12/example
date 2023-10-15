5a05afd892a60a96e430582ecc09a5fc53a6fc9a
ChoKyungHo <sbi06193@naver.com>
2023-10-15 10:52:19 +0900

    add c

--- src/sub.java
+++ src/sub.java
@@ -1,9 +1,11 @@
 public class sub {
     private int a;
     private int b;
+    private int c;
 
-    public sub(int a, int b) {
+    public sub(int a, int b, int c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
 }
